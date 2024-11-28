a9a49e93dc630e7712abdbba89c32851b6dac2a1
Lucasameye <lucas.ameye@icloud.com>
2024-11-28 11:30:25 +0100

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,30 +1,32 @@
-mijn_prijzen = {
-    "aardbei" : 3,
-    "vanille" : 4,
-    "chocolade" : 5,
-    
-}
-aanbieding = mijn_prijzen["vanille"]*0.8
-reclame_tekst = f"Vandaag in de aanbeiding: vanille-ijs, 1 liter - slechts ${aanbieding}"
+from helper import decoreer
 
-reclame_tekst2 = reclame_tekst[58]
+def print_aanbieding():
+    mijn_prijzen = {
+        "aardbei" : 3,
+        "vanille" : 4,
+        "chocolade" : 5,
+        
+    }
+    aanbieding = mijn_prijzen["vanille"]*0.8
+    reclame_tekst = f"Vandaag in de aanbeiding: vanille-ijs, 1 liter - slechts ${aanbieding}"
 
-reclame_tekst3 = reclame_tekst.upper()
+    reclame_tekst2 = reclame_tekst[:63]
 
-reclame_tekst4 = reclame_tekst3
-for el in reclame_tekst4: 
-    if len(el) > 4:
-        print(el.upper())
-    else:
-        print(el.lower())
+    reclame_tekst3 = reclame_tekst.upper()
 
-tekst="header"
-lengte = len(tekst) + 4
-print()
-print(lengte * "*")
-print(f"* {tekst} *")
-print(lengte * "*")
-print()
+    reclame_tekst4 = reclame_tekst3.split()
+    for el in reclame_tekst4: 
+        if len(el) > 4:
+            print(el.upper())
+        else:
+            print(el.lower())
+decoreer("aanbieding")
+print_aanbieding()  
+
+         
+
+
+
 
 
     
